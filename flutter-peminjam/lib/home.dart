070d7ead664a01200a6e641e824fc5abee5228f1
alghanaufal <95957498+alghanaufal@users.noreply.github.com>
2023-06-06 12:56:05 +0700

    Update home.dart detail

--- flutter-peminjam/lib/home.dart
+++ flutter-peminjam/lib/home.dart
@@ -1184,56 +1184,108 @@ class _SettingsState extends State<Settings> {
   }
 }
 
+
 class DataPribadi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(16),
-      child: Stack(
-        alignment: Alignment.topRight,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data Pribadi'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.white,
-              border: Border.all(
-                color: Colors.grey,
-                width: 2,
-              ),
-            ),
-            child: CircleAvatar(
-              radius: 45,
-              backgroundImage: NetworkImage(
-                  "https://media.licdn.com/dms/image/C4E03AQFnPXx0yxT4Uw/profile-displayphoto-shrink_800_800/0/1631632219753?e=2147483647&v=beta&t=4aWGfU5RFdkYI0N1gtVXIyTSYeNEoGdeIh2PlVi50xc"),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Halo,',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'Nopal',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
             ),
           ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: FractionalTranslation(
-              translation: Offset(0, 0.5),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 20),
-                  Text(
-                    'Nopal',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+          SizedBox(height: 16),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Informasi Akun',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 45,
+                      // Your profile image or icon can be added here
+                      backgroundImage: NetworkImage(
+                        "https://media.licdn.com/dms/image/C4E03AQFnPXx0yxT4Uw/profile-displayphoto-shrink_800_800/0/1631632219753?e=2147483647&v=beta&t=4aWGfU5RFdkYI0N1gtVXIyTSYeNEoGdeIh2PlVi50xc",
+                      ),
                     ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                ListTile(
+                  title: Text('Email: nopal@nopal.com'),
+                ),
+                ListTile(
+                  title: Text('Ganti Password'),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Informasi Pribadi',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
                   ),
-                  SizedBox(height: 8),
-                  Text(
-                    'nopal@nopal.com',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    '08123456789',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(height: 16),
+                ListTile(
+                  title: Text('Nama: Nopal'),
+                ),
+                ListTile(
+                  title: Text('No Telp: 0000000'),
+                ),
+                ListTile(
+                  title: Text('NIK: 000000000000'),
+                ),
+              ],
             ),
           ),
         ],
@@ -1241,3 +1293,4 @@ class DataPribadi extends StatelessWidget {
     );
   }
 }
+
