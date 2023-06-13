0008747050ad62a631251e3a15c41b30f4d6af11
MuhammadFahru <fahrumuhammad.ozi@gmail.com>
2023-06-13 11:02:41 +0700

    Coba Auth

--- flutter-investor/lib/main.dart
+++ flutter-investor/lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter_app/pages/password_sent.dart';
 import 'package:flutter_app/pages/signup.dart';
 import 'package:flutter_app/pages/index.dart';
 import 'package:flutter_app/pages/topUp.dart';
+import 'package:flutter_app/pages/sign_in.dart';
+import 'package:flutter_app/pages/sign_up.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +34,8 @@ class MyApp extends StatelessWidget {
             '/passwordSent': (context) => PasswordSentPage(),
             '/index': (context) => IndexPage(),
             '/topUp': (context) => TopUpPage(),
+            '/sign-in': (context) => SignInPage(),
+            '/sign-up': (context) => SignUpPage(),
           },
         ));
   }
--- flutter-peminjam/lib/main.dart
+++ flutter-peminjam/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
+import 'package:flutter_app/pages/home.dart';
+import 'package:flutter_app/pages/sign_in.dart';
+import 'package:flutter_app/pages/sign_up.dart';
 import 'package:flutter_app/pages/signup.dart';
 import 'package:flutter_app/pages/login.dart';
 import 'package:flutter_app/pages/forgot_password.dart';
 import 'package:flutter_app/pages/index.dart';
 import 'package:flutter_app/pages/password_sent.dart';
+import 'package:flutter_app/providers/auth_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,15 +24,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
         providers: [
-          BlocProvider<LoginCubit>(create: (context) => LoginCubit()),
+          // BlocProvider<LoginCubit>(create: (context) => LoginCubit()),
+          ChangeNotifierProvider(
+            create: (context) => AuthProvider(),
+          ),
         ],
         child: MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Prospera',
           theme: ThemeData(primarySwatch: Colors.deepPurple),
-          // debugShowCheckedModeBanner: false,
           initialRoute: '/',
           routes: {
-            '/': (context) => LoginPage(),
+            '/': (context) => IndexPage(),
+            '/sign-in': (context) => SignInPage(),
+            '/sign-up': (context) => SignUpPage(),
             '/forgotPassword': (context) => ForgotPasswordPage(),
             '/signUp': (context) => Signup(),
             '/passwordSent': (context) => PasswordSentPage(),
