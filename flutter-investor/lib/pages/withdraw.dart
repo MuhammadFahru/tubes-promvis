98d9d690c2941fbbaa10b161d855ef6a3bdf4362
davinUpi <davin1fausta@upi.edu>
2023-06-15 11:22:55 +0700

    basic withdraw & fixes

--- flutter-investor/lib/pages/withdraw.dart
+++ flutter-investor/lib/pages/withdraw.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_app/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WithdrawCubit extends Cubit<WithdrawState> {
-  WithdrawCubit() : super(WithdrawState(banks: ['Bank A', 'Bank B', 'Bank C']));
+  WithdrawCubit() : super(WithdrawState());
   final formKey = GlobalKey<FormState>();
   final customNominalController = TextEditingController();
 
@@ -39,15 +41,13 @@ class WithdrawCubit extends Cubit<WithdrawState> {
 }
 
 class WithdrawState {
-  final List<String> banks;
   final String selectedBank;
   final int selectedNominal;
   final int customNominal;
   final String nominalFieldVal;
 
   WithdrawState(
-      {required this.banks,
-      this.selectedBank = '',
+      {this.selectedBank = '',
       this.selectedNominal = 0,
       this.customNominal = 0,
       this.nominalFieldVal = 'test'});
@@ -60,7 +60,6 @@ class WithdrawState {
     String? nominalFieldVal,
   }) {
     return WithdrawState(
-      banks: banks ?? this.banks,
       selectedBank: selectedBank ?? this.selectedBank,
       selectedNominal: selectedNominal ?? this.selectedNominal,
       customNominal: customNominal ?? this.customNominal,
@@ -69,207 +68,252 @@ class WithdrawState {
   }
 }
 
-class withdrawPage extends StatelessWidget {
+class WithdrawPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<WithdrawCubit>(
       create: (context) => WithdrawCubit(),
       child: Scaffold(
+          backgroundColor: Colors.white,
           appBar: AppBar(
-            title: Text('Withdraw'),
+            iconTheme: IconThemeData(color: Colors.black),
+            centerTitle: true,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            toolbarHeight: 80,
+            title: Text(
+              'Top Up',
+              style: GoogleFonts.inter(
+                  fontWeight: semiBold, fontSize: 30, color: Colors.black),
+            ),
           ),
-          body: TopUpForm()),
+          body: WithdrawForm()),
     );
   }
 }
 
-class TopUpForm extends StatelessWidget {
+class WithdrawForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
-          SizedBox(height: 32),
-          const Align(
+          SizedBox(
+            height: 32.0,
+          ),
+          // nominal box
+          _buildNominalCards(context),
+          SizedBox(
+            height: 64.0,
+          ),
+          // nominal manual
+          _buildNominalField(context),
+          const Spacer(),
+          _buildTopUpButton(
+            context,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNominalCards(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        children: [
+          Align(
             alignment: Alignment.centerLeft,
             child: Padding(
-              padding: EdgeInsets.only(left: 10),
-              child: Text(
-                'Pilih bank:',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 10, left: 10, right: 10),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: DropdownButtonHideUnderline(
-                child: Container(
-              margin: EdgeInsets.only(left: 10, right: 10),
-              child: DropdownButton<String>(
-                isExpanded: true,
-                value: context.watch<WithdrawCubit>().state.selectedBank,
-                onChanged: (value) =>
-                    context.read<WithdrawCubit>().selectBank(value!),
-                items: [
-                  if (context.watch<WithdrawCubit>().state.selectedBank.isEmpty)
-                    const DropdownMenuItem(
-                      value: '',
-                      child: Text('Pick a bank'),
-                    ),
-                  ...context
-                      .watch<WithdrawCubit>()
-                      .state
-                      .banks
-                      .map((bank) => DropdownMenuItem(
-                            value: bank,
-                            child: Text(bank),
-                          ))
-                      .toList(),
-                ],
-              ),
-            )),
-          ),
-          const SizedBox(height: 50.0),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.only(left: 10),
+              padding: const EdgeInsets.only(left: 10),
               child: Text(
                 'Pilih nominal:',
-                style: TextStyle(fontWeight: FontWeight.bold),
+                style: GoogleFonts.inter(
+                    color: Colors.black, fontSize: 14, fontWeight: semiBold),
               ),
             ),
           ),
-          const SizedBox(height: 32.0),
-          // nominal box
-          Row(
-            children: [
-              Expanded(
-                child: BlocBuilder<WithdrawCubit, WithdrawState>(
-                  builder: (context, state) {
-                    final nominals = [
-                      5000,
-                      10000,
-                      25000,
-                      50000,
-                      100000,
-                      150000
-                    ];
-                    return GridView.count(
-                      crossAxisCount: 3,
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      childAspectRatio: 2.5,
-                      children: nominals.map((nominal) {
-                        return Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12.0),
-                            side: BorderSide(
-                              color: state.selectedNominal == nominal
-                                  ? Colors.purple
-                                  : Colors.transparent,
-                              width: 2.0,
-                            ),
-                          ),
-                          child: InkWell(
-                            onTap: () {
-                              final cubit = context.read<WithdrawCubit>();
-                              if (state.selectedNominal == nominal) {
-                                cubit.selectNominal(0);
-                              } else {
-                                cubit.selectNominal(nominal);
-                              }
-                            },
-                            child: Stack(
-                              alignment: Alignment.topRight,
-                              children: [
-                                Container(
-                                  alignment: Alignment.center,
-                                  child: Text(
-                                    "Rp${nominal.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}",
-                                    style: TextStyle(
-                                      color: state.selectedNominal == nominal
-                                          ? Colors.purple
-                                          : Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                state.selectedNominal == nominal
-                                    ? Container(
-                                        width: 24,
-                                        height: 24,
-                                        margin: EdgeInsets.all(8),
-                                        decoration: BoxDecoration(
-                                          color: Colors.purple,
-                                          borderRadius:
-                                              BorderRadius.circular(12.0),
-                                        ),
-                                        child: Icon(
-                                          Icons.check,
-                                          color: Colors.white,
-                                          size: 16,
-                                        ),
-                                      )
-                                    : SizedBox(),
-                              ],
-                            ),
-                          ),
-                        );
-                      }).toList(),
-                    );
+          SizedBox(
+            child: GridView.count(
+              crossAxisCount: 3,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(10),
+              childAspectRatio: 2.0,
+              children: [
+                10000,
+                20000,
+                50000,
+                100000,
+                250000,
+                500000,
+              ].map((nominal) {
+                final isSelected = context.select((WithdrawCubit cubit) =>
+                    cubit.state.selectedNominal == nominal);
+
+                return GestureDetector(
+                  onTap: () {
+                    final cubit = context.read<WithdrawCubit>();
+                    if (isSelected) {
+                      cubit.selectNominal(0);
+                    } else {
+                      cubit.selectNominal(nominal);
+                    }
                   },
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 16.0),
-          BlocBuilder<WithdrawCubit, WithdrawState>(builder: (context, state) {
-            return TextFormField(
-              controller: context.read<WithdrawCubit>().customNominalController,
-              validator: (value) {
-                if ((value == null || value.isEmpty) &&
-                    state.selectedNominal == 0) {
-                  return 'Please enter a nominal value';
-                } else if (value != null &&
-                    value.isNotEmpty &&
-                    int.parse(value) <= 0) {
-                  return 'Please enter a value higher than 0';
-                }
-                return null;
-              },
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                context.read<WithdrawCubit>().customNominal(value);
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter Nominal',
-              ),
-            );
-          }),
-          const Spacer(),
-          ElevatedButton(
-            onPressed: (context.watch<WithdrawCubit>().state.selectedNominal !=
-                            0 ||
-                        context.watch<WithdrawCubit>().state.customNominal !=
-                            0) &&
-                    context.watch<WithdrawCubit>().state.selectedBank != ''
-                ? () => context.read<WithdrawCubit>().topUp()
-                : null,
-            child: Text('Finalize'),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      side: BorderSide(
+                        color: isSelected ? primaryColor : Colors.transparent,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Rp${nominal.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}",
+                            style: GoogleFonts.inter(
+                                color: isSelected ? primaryColor : Colors.grey,
+                                fontWeight: medium,
+                                fontSize: 16),
+                          ),
+                        ),
+                        if (isSelected)
+                          Positioned(
+                            top: 4,
+                            right: 4,
+                            child: Container(
+                              width: 15,
+                              height: 15,
+                              decoration: BoxDecoration(
+                                color: primaryColor,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Icon(
+                                Icons.check,
+                                color: Colors.white,
+                                size: 12,
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildNominalField(BuildContext context) {
+    final cubit = context.read<WithdrawCubit>();
+    final customNominalController = cubit.customNominalController;
+
+    return BlocBuilder<WithdrawCubit, WithdrawState>(
+      builder: (context, state) {
+        final selectedNominal = state.selectedNominal;
+        final isCustomNominalEmpty = customNominalController.text.isEmpty;
+
+        String? validateNominal(String? value) {
+          if (isCustomNominalEmpty && selectedNominal == 0) {
+            return 'Please enter a nominal value';
+          } else if (!isCustomNominalEmpty && int.parse(value!) <= 0) {
+            return 'Please enter a value higher than 0';
+          }
+          return null;
+        }
+
+        return Container(
+          decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(16.0)),
+          child: TextFormField(
+            controller: customNominalController,
+            validator: validateNominal,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            inputFormatters: [
+              FilteringTextInputFormatter.digitsOnly,
+            ],
+            keyboardType: TextInputType.number,
+            onChanged: cubit.customNominal,
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+              border: OutlineInputBorder(
+                borderSide: BorderSide.none,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              filled: true,
+              fillColor: backgroundColor6,
+              label: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(
+                    Icons.add_box,
+                    color: Colors.grey,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    'Input Manual',
+                    style: GoogleFonts.inter(
+                        color: secondaryTextColor,
+                        fontSize: 16,
+                        fontWeight: medium),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildTopUpButton(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20.0),
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primaryColor,
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+        onPressed: (context.watch<WithdrawCubit>().state.selectedNominal != 0 ||
+                    context.watch<WithdrawCubit>().state.customNominal != 0) &&
+                context.watch<WithdrawCubit>().state.selectedBank != ''
+            ? () => context.read<WithdrawCubit>().topUp()
+            : null,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Text(
+            'Finalize',
+            style: GoogleFonts.inter(
+                color: (context.watch<WithdrawCubit>().state.selectedNominal !=
+                                0 ||
+                            context
+                                    .watch<WithdrawCubit>()
+                                    .state
+                                    .customNominal !=
+                                0) &&
+                        context.watch<WithdrawCubit>().state.selectedBank != ''
+                    ? Colors.white
+                    : Colors.grey[500],
+                fontWeight: semiBold,
+                fontSize: 16),
+          ),
+        ),
       ),
     );
   }
